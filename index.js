beb1894c87f92dc5f8e66ba27b9511253d56dbec
Farjana Yeasmin Rimi <farjanayeasmin003@gmail.com>
2026-06-29 20:10:31 +0600

    configure MongoDB client

--- index.js
+++ index.js
@@ -10,3 +10,15 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
+const uri = process.env.MONGODB_URI;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
